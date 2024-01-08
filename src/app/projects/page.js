c739403ac4c9e29af1709b56e657845e57e96bfc
yaghoubi-m <yaghoubifrontdev@gmail.com>
2024-01-09 01:14:50 +0330

    final

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -5,7 +5,7 @@ import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/post
 
 async function getProjects() {
   try {
-    const res = await fetch(`${process.env.BASEURL}/api/Project/CompleteProjectsList`, {cache: 'force-cache'})
+    const res = await fetch(`${process.env.BASEURL}/api/Project/CompleteProjectsList`, )
     return res.json()
   }catch (e){
     console.log(e)
